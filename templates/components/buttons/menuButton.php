b2c54d345290e80bfd5b33ac2e6641ad9058af61
Paul Ashby <paul@primitive.co>
2020-04-18 21:01:44 +0100

    menus

---
+++ templates/components/buttons/menuButton.php
@@ -0,0 +1,7 @@
+<?php
+
+return "<div class='{$button_class}'>
+	<div class='menu__bar menu__bar--1'></div>
+	<div class='menu__bar menu__bar--2'></div>
+	<div class='menu__bar menu__bar--3'></div>
+</div>";
