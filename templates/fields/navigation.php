16784073c87553efd0fb26713f0c07d9a25f583a
Paul Ashby <paul@primitive.co>
2020-05-08 17:25:33 +0100

    replace include_button with sliding var

--- templates/fields/navigation.php
+++ templates/fields/navigation.php
@@ -1,5 +1,4 @@
 <?php
-
 // Use defaults if no options provided
 if( ! is_array($value)) {
 	$value = array(
@@ -11,19 +10,19 @@ if( ! is_array($value)) {
 	        "outer_tpl" => "<ul class='nav__top-level'>||</ul>",
 	        "inner_tpl" => "<ul class='nav__dropdown'>||</ul>",
 	    ), 
-	    "include_button" => false
+	    "sliding" => false
     );
 }
 
 $tree_menu = $modules->get("MarkupSimpleNavigation");
-$include_button = $value["include_button"];
+$sliding = $value["sliding"];
 $tree_options = $value["tree_options"];
 
 // Hook for custom item string
 // http://modules.processwire.com/modules/markup-simple-navigation/
-// Used anonymous function so I could pass in $include_button var
+// Used anonymous function so I could pass in $sliding var
 // https://processwire.com/talk/topic/14879-solved-can-i-pass-a-variable-to-a-hook-function/
-$tree_menu->addHookAfter('getItemString', function($event) use($include_button) {
+$tree_menu->addHookAfter('getItemString', function($event) use($sliding) {
 	
      $child = $event->arguments('page'); // current rendered child page
     // any logic with $child possible here
@@ -33,17 +32,17 @@ $tree_menu->addHookAfter('getItemString', function($event) use($include_button) 
     if($is_level_1){
     	
         // set the return value of this hook to a custom string
-        $event->return = getLevel1($child, $include_button);
+        $event->return = getLevel1($child, $sliding);
     }
 });
 
 return "<nav class='nav'>" . $tree_menu->render($tree_options) . "</nav>";
 
-function getLevel1 ($child, $include_button) {
+function getLevel1 ($child, $sliding) {
 	$cat = $child->title;
 	$cat_lc = str_replace(' ', '', strtolower($cat));
 	$link = $child->url();
-	$button_str = $include_button ? " nav__top-cat--buttoned" : "";
+	$button_str = $sliding ? " nav__top-cat--buttoned" : "";
 
 	if($child->hasChildren()) {
 		return "<a href='$link' class='nav__top-cat nav__top-cat--$cat_lc $button_str' data-cat='$cat_lc'>$cat</span></a>";
