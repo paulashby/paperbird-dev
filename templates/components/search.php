34add8fce3e2e85d3bd54b25f7514e4f9909385b
Paul Ashby <paul@primitive.co>
2020-05-15 17:11:10 +0100

    update import statements to refelct new file names

--- templates/components/search.php
+++ templates/components/search.php
@@ -1,9 +1,13 @@
 <?php
 // This form is from DAM system - Sites/r.io/templates/components/navigation.php
 
-$out = "<h2><a href='#' class='menu__entrybutton menu__entrybutton--search' data-buttontype='search'>Search</a></h2>
-			<form class='search__form' action='/' method='get'>
-				<input class='' type='text' name='q' id='search_query' value='Search'/>
-			</form>";
+// $out = "<h2><a href='#' class='menu__entrybutton menu__entrybutton--search' data-buttontype='search'>Search</a></h2>
+// <div class='search'>
+// <form class='search__form' action='/' method='get'>
+// 		<input class='' type='text' name='q' id='search_query' value='Search'/>
+// 	</form>
+// </div>";
+$out = "<h2><a href='#'  class='menu__entrybutton menu__entrybutton--search' data-buttontype='search'>Search</a></h2>
+<ul class='search'><li>Search here</li></ul>";
 
 return $out;
