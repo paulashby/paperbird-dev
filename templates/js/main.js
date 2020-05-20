b0b9f844f50c5ad14f47e794a88f1c50fae373b8
Paul Ashby <paul@primitive.co>
2020-05-20 20:22:48 +0100

    add error handling

--- templates/js/main.js
+++ templates/js/main.js
@@ -1,5 +1,6 @@
 // import { customEventPolyfill } from './helpers';
 import $ from 'jquery';
+import errorHandler from './utilities/errorHandler';
 import menu from './modules/menu';
 import navigation from './modules/navigation';
 //TODO: May need to move form.js import and initialisation out of menu.js if other elements of the page include forms
@@ -15,6 +16,10 @@ function init () {
 
 	let sliding = $('.nav').hasClass('nav--sliding');
 
+	let error_handler_settings = {
+		url: config.errorHandlerURL
+	};
+
 	let nav_settings = {
 		menu: menu,
 		sliding: sliding
@@ -23,12 +28,12 @@ function init () {
 	let menu_settings = {
 
 		sliding: sliding,
-		toggleSubmenu: navigation.toggleSubmenu,
+		navigationClick: navigation.navigationClick,
 		resetNavDropdown: navigation.resetDropdown,
 		closeNavDropdown: navigation.closeDropdown
 
 	}
-
+	errorHandler.init(error_handler_settings);
 	menu.init(menu_settings);
 	navigation.init(nav_settings);	
 	
