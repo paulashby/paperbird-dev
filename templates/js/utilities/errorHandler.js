18efa78424d1826443865506efe2de608a1a5a11
Paul Ashby <paul@primitive.co>
2020-05-21 16:48:22 +0100

    downgrade console error messages to warnings

--- templates/js/utilities/errorHandler.js
+++ templates/js/utilities/errorHandler.js
@@ -2,32 +2,30 @@ function init (settings) {
 
 	window.addEventListener('error', function (e) {
 
-    let ajaxurl = settings.url;
-    let stack = e.error ? e.error.stack : 'stack unavailable';
-    let ajaxdata = {
-        error: e.error,
-        message: e.message,
-        stack: stack
-    };
+        let ajaxurl = settings.url;
+        let stack = e.error ? e.error.stack : 'stack unavailable';
+        let ajaxdata = {
+            error: e.error,
+            message: e.message,
+            stack: stack
+        };
 
-    
-    $.ajax({
-        type: 'post', 
-        url: ajaxurl,
-        data: ajaxdata,
-        dataType: 'json',
-        success: function(data) {
-        	if(data.message) {
-        		console.log(e.message + ": " + data.message);	
-        	}
-       },
-        error: function(jqXHR, textStatus, errorThrown) {
-        	console.log(textStatus, errorThrown);
-        } 
+        $.ajax({
+            type: 'post', 
+            url: ajaxurl,
+            data: ajaxdata,
+            dataType: 'json',
+            success: function(data) {
+            	if(data.message) {
+            		console.warn(e.message + ": " + data.message);	
+            	}
+           },
+            error: function(jqXHR, textStatus, errorThrown) {
+            	console.warn(textStatus, errorThrown);
+            } 
+        });
+
     });
-
-});
-
 }
 
 const errorHandler = {
