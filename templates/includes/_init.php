4cfc83d9ebfab8ea82a5133b66b8a01dcd574761
Paul Ashby <paul@primitive.co>
2020-05-30 19:21:57 +0100

    add hooks for forgotten password form fields. Style forgotten password form

--- templates/includes/_init.php
+++ templates/includes/_init.php
@@ -1,3 +1,35 @@
 <?php
 
 include_once("./_func.php");
+
+if($page->template->name === 'services-forgotten-password') {
+
+	$wire->addHookBefore('Inputfield::render', function($event) {
+		$inputfield = $event->object;
+
+		if($inputfield instanceof InputfieldText) {
+
+			$inputfield->addClass('form__input form__input--validate form__input--email');
+			$inputfield->attr('pattern', '^([\w\-\.]+)@((\[([0-9]{1,3}\.){3}[0-9]{1,3}\])|(([\w\-]+\.)+)([a-zA-Z]{2,4}))$');
+
+		} else if($inputfield instanceof InputfieldSubmit) {
+			// submit button
+			$inputfield->addClass('form__button form__button--submit');
+		}
+	}); 
+
+	$this->addHookAfter('Inputfield::render', function(HookEvent $event) {
+		  
+		  $inputfield = $event->object;
+
+		  $return = $event->return;
+
+		  if($inputfield instanceof InputfieldText) {
+
+			$return .= "<label class='form__error'>Please enter a valid email address</label>";
+
+		}
+		$event->return = $return;
+		});
+
+}
