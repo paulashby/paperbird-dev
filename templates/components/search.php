2ad0e7e0e5b166ba6f6717bd4fa36236f8ecac9d
Paul Ashby <paul@primitive.co>
2020-07-13 19:42:07 +0100

    Implement search

--- templates/components/search.php
+++ templates/components/search.php
@@ -1,7 +1,7 @@
 <?php namespace ProcessWire;
 
 $search_button = $files->render('components/buttons/menuToolButton', ['button_text'=>'Search', 'button_class'=>'menu__entrybutton menu__entrybutton--search', 'button_type'=>'search']);
-$submit_button = $files->render('components/buttons/formButton', ['button_class'=>'form__button form__button--submit', 'button_type'=>'submit', 'action'=>'submit']);
+$submit_button = $files->render('components/buttons/formButton', ['button_class'=>'form__button form__button--submit', 'button_type'=>'submit', 'action'=>'search']);
 $cancel_button = $files->render('components/buttons/formButton', ['button_class'=>'form__button form__button--cancel', 'button_type'=>'button', 'action'=>'cancel']);
 
 $out = "<h2>{$search_button}</h2>
@@ -13,6 +13,7 @@ $out = "<h2>{$search_button}</h2>
 			{$cancel_button}
 		</div>
 	</form>
+	<p class='form__error form__error--submission'>No Error</p>
 </div>";
 
 return $out;
