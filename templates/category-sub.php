762719b316bf7e9abfa5984a866edd0ec8ac41cc
Paul Ashby <paul@primitive.co>
2020-10-03 08:41:48 +0100

    Update product listing, add lightbox

--- templates/category-sub.php
+++ templates/category-sub.php
@@ -1,20 +1,48 @@
 <?php namespace ProcessWire;
 
+
+
+// We actually want to render the image, sku and title then load the lightbox via an ajax call when a card is clicked
+
 $cart = $this->modules->get("OrderCart");
 
-if( ! $config->ajax) {
+if( $config->ajax) {
 
-	$products = $page->children();
-	$products_out = "";
+	// Lightbox interaction
 
-	$title = $page->title;
+	$sku = $input->get("sku", "text");
 
-	foreach ($products as $product) {
-		$products_out .= $cart->renderItem($product);
+	if($sku && $user->isLoggedin()) {
+
+		// Get lightbox markup
+		$lightbox = $files->render('components/lightbox', ['sku'=>$sku, 'cart'=>$cart, 'action'=>'toggleMenuDisplay']);	
+
+		return json_encode(array("success"=>true, "data"=>$lightbox));
+
+	} else {
+
+		return json_encode(array("success"=>false, "error"=>"Users must be logged in to use the cart"));
 	}
 
-	echo "<main data-pw-id='main'>
-	<h1>Hello, this is the $title page</h1>
-	$products_out
-	</main>";
 }
+
+$category = $page->parent->title;
+$title = $page->title;
+$products = $page->children();
+$products_out = "";
+$size = $cart->getProductShotSize(true);
+
+foreach ($products as $product) {
+	$product_shot = $product->product_shot->first();
+	$product_shot_url = $product_shot->size($size, $size)->url;
+	$dsc = $product_shot->description;
+	$alt_text = $dsc ? $dsc : $product->title;
+	$sku = $product->sku;
+	$product_title = $product->title;
+	$products_out .= "<div class='product' data-action='toggleLightbox' data-sku='$sku'><img src='$product_shot_url' alt='$alt_text'><p>$sku</p><h2>$product_title</h2></div>";
+}
+echo "<main data-pw-id='main'>
+	<h1>$category $title</h1>
+	$products_out
+	<div class='card-viewer'></div>
+	</main>";
