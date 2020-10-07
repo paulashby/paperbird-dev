f0fdf4fc07684f94a12d51569f5ef2c28bbac80f
Paul Ashby <paul@primitive.co>
2020-10-07 18:47:27 +0100

    Place variations in ul and remove outer anchors so we can load variation content into lightbox instead of opening its page

--- templates/components/lightbox.php
+++ templates/components/lightbox.php
@@ -1,28 +1,38 @@
 <?php namespace ProcessWire;
 
-$lightbox = "<div class='lightbox'>";
-$lightbox .= $files->render('components/buttons/toggleButton', ['button_class'=>'toggle__button toggle__button--close toggle__button--lightbg', 'action'=>'closeLightbox']);
+$lightbox_inner = $files->render('components/buttons/toggleButton', ['button_class'=>'toggle__button toggle__button--close toggle__button--lightbg', 'action'=>'closeLightbox']);
+$lightbox_inner .= "<div class='active-card'>";
+
 $product_page = $pages->get("sku=$sku");
-$lightbox .= $cart->renderItem($product_page);
-$lightbox .= "<p class='lightbox__size'>XXX x XXXmm";
-$lightbox .= "<p class='lightbox__paperspec'>XXXgsm uncoated</p>";
-$lightbox .= "<p>Do we want fields for size and paper stock? Or do we base the former on the price category and the latter just a site-wide setting?</p>";
+$lightbox_inner.= $cart->renderItem($product_page);
 
-$lightbox .= "<div class='lightbox__variations'>";
+$lightbox_inner .= "<p class='lightbox__size'>XXX x XXXmm";
+$lightbox_inner .= "<p class='lightbox__paperspec'>XXXgsm uncoated</p>";
+$lightbox_inner .= "<p>Do we want fields for size and paper stock? Or do we base the former on the price category and the latter just a site-wide setting?</p>
+</div><!-- End active-card -->";
 
-foreach ($product_page->variation as $product) {
-	$p_url = $product->url;
-	$product_shot = $product->product_shot;
-	$size = 60;
-	$product_shot_url = $product_shot->first()->size($size, $size)->url;
-	$dsc = $product_shot->description;
-	$alt_text = $dsc ? $dsc : $product->title;
+$lightbox_class = "lightbox";
 
-	//TODO: We actually want to load variation pages into the lightbox rather than loading  pages
-	$lightbox .= "<a href='$p_url' class='lightbox__variation'><img class='lightbox__variation-img' src='$product_shot_url' alt='$alt_text'></a>";
+if(count($product_page->variations)){
+
+	$lightbox_class .= " lightbox--has-variations";
+
+	$lightbox_inner .= "<ul class='lightbox__variations'>";
+
+	foreach ($product_page->variations as $product) {
+		$p_sku = $product->sku;
+		$product_shot = $product->product_shot;
+		$size = 80;
+		$product_shot_url = $product_shot->first()->size($size, $size)->url;
+		$dsc = $product_shot->description;
+		$alt_text = $dsc ? $dsc : $product->title;
+
+		$lightbox_inner .= "<li class='lightbox__variation'><img class='lightbox__variation-img' src='$product_shot_url' alt='$alt_text' data-action='showProduct' data-sku='$p_sku'></li>";
+	}
+	$lightbox_inner .= "</ul>";
 }
-$lightbox .= "</div>
-</div>";
+
+$lightbox = "<section class='$lightbox_class'>$lightbox_inner</section>";
 
 return $lightbox;
 
