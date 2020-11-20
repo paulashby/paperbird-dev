045ea50355466dbdbbb108ebc636794ccb666c76
Paul Ashby <paul@primitive.co>
2020-11-20 19:14:30 +0000

    Remove redundant comment

--- templates/fields/titleBlock.php
+++ templates/fields/titleBlock.php
@@ -6,7 +6,6 @@ echo "<div class='titleBlock'>
     <img class='titleBlock__logo' src='" . $page->logo->url . "' alt='Paperbird logo'>
     <h1 class='titleBlock__title'>Delightfully cute &amp; quirky</h1>";
 
-    // Menu
     $menu_options = array(
         "navigation" => true,
         "search" => true,
