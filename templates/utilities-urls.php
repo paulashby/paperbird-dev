e6cdaf2b572d7807302d5157060680d8556b0bd5
Paul Ashby <paul@primitive.co>
2020-11-26 21:18:36 +0000

    Remove search url as this is now handled by templates/utilities-ajax-interactions

--- templates/utilities-urls.php
+++ templates/utilities-urls.php
@@ -6,6 +6,5 @@ return array(
     "logInOutURL" => $pages->get(1017)->url,
     "errorHandlerURL" => $pages->get(1084)->url,
     "forgotPassword" => $pages->get(1089)->url,
-    "searchURL" => $pages->get(1274)->url,
     "ajaxURL" => $pages->get(1323)->url
 );
