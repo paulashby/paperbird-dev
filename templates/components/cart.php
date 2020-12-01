82353f6bfcec8f5a468d3d53b5b9a14c60ecd61a
Paul Ashby <paul@primitive.co>
2020-12-01 12:06:37 +0000

    Add counter for cart items

--- templates/components/cart.php
+++ templates/components/cart.php
@@ -1,12 +1,21 @@
 <?php namespace ProcessWire;
 
 $cart = $this->modules->get("OrderCart");
+$counter = "";
+if($value["counter"]){
+	$count = $cart->getNumCartItems();
+	$class_suffix = $count ? " cart__counter-count--show" : ""; 
+	$counter .= "<p class='cart__counter'><span id='counter' class='cart__counter-count$class_suffix'>$count</span></p>";
+}
 $cart_button = $files->render('components/buttons/menuToolButton', ['button_text'=>'Cart', 'button_class'=>'menu__entrybutton menu__entrybutton--cart', 'button_type'=>'cart']);
 $cancel_button = $files->render('components/buttons/formButton', ['button_class'=>'form__button form__button--cancel', 'button_type'=>'button', 'label'=>'Continue shopping', 'action'=>'cancel']);
 
 $cart_out = $cart->renderEmptyCart();
 
-$out = "<h2>$cart_button</h2>
+$out = "<div>
+	$counter
+	$cart_button
+</div>
 <div class='cart'>
 $cart_out
 $cancel_button
