f292936d9a26f508d58c290f4c6e5f37b4a52e20
Paul Ashby <paul@primitive.co>
2020-12-02 18:28:54 +0000

    Provide listing entries with custom images to OrderCart module

---
+++ templates/components/customCartImages.php
@@ -0,0 +1,18 @@
+<?php namespace ProcessWire;
+
+// Expects $product and $img_count vars
+
+$lazyImages = $this->modules->get("LazyResponsiveImages");
+$max_eager = (int) $lazyImages->getMaxEager("cart");
+$lazy_load = $img_count > $max_eager;
+
+$listing_options = [
+	"lazyImages"=>$lazyImages,
+    "product"=>$product,
+    "field_name"=>"product_shot",
+    "sizes"=>"(max-width: 1000px) 150px, 100px",
+    "class"=>"products__product-shot",
+    "lazy_load"=>$lazy_load
+];
+
+return $this->files->render("components/productListingEntry", $listing_options);
