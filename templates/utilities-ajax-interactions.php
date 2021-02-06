e8ed00eca3300e160667cb9477cdeabcd60f6b1c
Paul Ashby <paul@primitive.co>
2021-02-06 17:33:30 +0000

    Throw exception if not ajax. Adjust indentation

--- templates/utilities-ajax-interactions.php
+++ templates/utilities-ajax-interactions.php
@@ -1,67 +1,69 @@
 <?php namespace ProcessWire;
 
-if( $config->ajax) {
+if( ! $config->ajax) {
+	// We shouldn't be here...
+	throw new Wire404Exception();
+}
 
-	$action = $input->get("action", "text");
+$action = $input->get("action", "text");
 
-	switch ($action) {
-		case "populateLightbox":
-			$sku = $input->get("sku", "text");
+switch ($action) {
+	case "populateLightbox":
+		$sku = $input->get("sku", "text");
 
-			if($sku) {
-				$cart = $this->modules->get("OrderCart");
-				// Get lightbox markup
-				$lightbox = $files->render('components/lightbox', ['sku'=>$sku, 'cart'=>$cart, 'action'=>'toggleMenuDisplay']);	
-
-				return json_encode(array("success"=>true, "data"=>$lightbox));
-
-			} 
-			return json_encode(array("success"=>false, "error"=>"Users must be logged in to use the cart"));
-
-		case "populateCart":
+		if($sku) {
 			$cart = $this->modules->get("OrderCart");
-			$cart_content = $cart->populateCart("components/customCartImages");
-			return json_encode(array("success"=>true, "data"=>$cart_content));
+			// Get lightbox markup
+			$lightbox = $files->render('components/lightbox', ['sku'=>$sku, 'cart'=>$cart, 'action'=>'toggleMenuDisplay']);	
 
-		case "search":
-			$q = $input->get('q'); 
-			$out = $files->render("components/search", array("value"=>array("container" => false)));
-			$no_results = "<h3>Your search returned no results</h3>";
+			return json_encode(array("success"=>true, "data"=>$lightbox));
 
-			if($q) {
+		} 
+		return json_encode(array("success"=>false, "error"=>"Users must be logged in to use the cart"));
 
-				$q = explode(" ", $sanitizer->text($q));
+	case "populateCart":
+		$cart = $this->modules->get("OrderCart");
+		$cart_content = $cart->populateCart("components/customCartImages");
+		return json_encode(array("success"=>true, "data"=>$cart_content));
 
-				foreach($q as $key => $value){
-					$reqs[$key] = $sanitizer->selectorValue($value);
+	case "search":
+		$q = $input->get('q'); 
+		$out = $files->render("components/search", array("value"=>array("container" => false)));
+		$no_results = "<h3>Your search returned no results</h3>";
+
+		if($q) {
+
+			$q = explode(" ", $sanitizer->text($q));
+
+			foreach($q as $key => $value){
+				$reqs[$key] = $sanitizer->selectorValue($value);
+			}
+
+			$search_term_string = implode("|", $q);
+			$selector = "template=product, title|tags|artist~=" . $search_term_string . ", limit=30";
+			$matches = $pages->find($selector);
+			$result_count = count($matches);
+
+			if($result_count) {
+
+				$cart = $this->modules->get("OrderCart");
+				$matches->sort("title");
+				$match_string = $result_count > 1 ? "$result_count matches" : "$result_count match";
+				$out .= "<h3>Search returned $match_string</h3>";
+
+				foreach ($matches as $match) {
+					//TODO: we want everything a lightbox entry gets - then these are all the customer needs to place an order!
+					$out .= $cart->renderItem($match);
 				}
 
-				$search_term_string = implode("|", $q);
-				$selector = "template=product, title|tags|artist~=" . $search_term_string . ", limit=30";
-				$matches = $pages->find($selector);
-				$result_count = count($matches);
-
-				if($result_count) {
-
-					$cart = $this->modules->get("OrderCart");
-					$matches->sort("title");
-					$match_string = $result_count > 1 ? "$result_count matches" : "$result_count match";
-					$out .= "<h3>Search returned $match_string</h3>";
-
-					foreach ($matches as $match) {
-						//TODO: we want everything a lightbox entry gets - then these are all the customer needs to place an order!
-						$out .= $cart->renderItem($match);
-					}
-
-				} else {
-					$out .= $no_results;
-				}
 			} else {
 				$out .= $no_results;
 			}
-			return json_encode(array("success"=>true, "data"=>$out));
-		
-		default:
-			return json_encode(array("success"=>false, "error"=>"Unknown AJAX action: '$action'"));
-	}
+		} else {
+			$out .= $no_results;
+		}
+		return json_encode(array("success"=>true, "data"=>$out));
+	
+	default:
+		return json_encode(array("success"=>false, "error"=>"Unknown AJAX action: '$action'"));
 }
