34d182a91dac480c8257dcef64e9bbadf63d3408
Paul Ashby <paul@primitive.co>
2021-03-31 18:07:58 +0100

    Throw exception if not ajax call. Use ProcessContactPages login function instead of calling session->login directly in order to prevent holders of unapproved accounts from logging in

--- templates/nav-login.php
+++ templates/nav-login.php
@@ -1,60 +1,55 @@
 <?php namespace ProcessWire;
 
 if( ! $config->ajax) {
-
-    // We shouldn't be here...
-    $session->redirect($pages->get(27)->url);
-
-} else {
-
-	if($input->post('logout')){
-
-		if($user->isLoggedin()) {
-
-			$session->logout();
-			return json_encode(array('success'=>true));
-
-		}
-	}
-	$errors = array();
-	$user_data = array();
-
-	if($input->post('email')) {
-		$user_data['email'] = $sanitizer->email($input->post('email'));
-		$user_data['user'] = $users->get('email=' . $this->sanitizer->selectorValue($user_data['email']));
-
-		if(0 === $user_data['user']->id) {
-			$errors[] = 'No such user';
-		}
-        if( ! $input->post('password')) {
-        	$errors[] = 'Password required';
-        } else {
-        	//TODO: Create a set of rules for password validation (not santization)length, required characters etc - same ruleset should be used on front end validation in form.js. Then validate this before storing.
-        	$user_data['password'] = $input->post('password');
-        	//TODO: Add password to errors if it failed validation
-        }
-	} else {
-		$errors[] = 'email required';
-	}
-
-	if (empty($errors)) {
-		$user = wire('session')->login($user_data['user'], $input->post('password'));
-
-		if($user) {
-			$cart = $this->modules->get("OrderCart");
-			$num_cart_items = $cart->getNumCartItems($user->id);
-			$data['num_cart_items'] = $num_cart_items;
-			$data['success'] = true;
-			$data['message'] = 'Welcome back'; 
-		} else {
-			$data['success'] = false;
-            $data['message'] = 'Login failed!';
-            $data['errors'][] = 'Incorrect password';
-        }   
-    } else {
-    	$data['success'] = false;
-        $data['errors']  = $errors;
-    }
+	// We shouldn't be here...
+    throw new Wire404Exception();
 }
 
-return json_encode($data);
+if($input->post('logout')){
+
+	if($user->isLoggedin()) {
+
+		$session->logout();
+		return json_encode(array('success'=>true));
+
+	}
+}
+$errors = array();
+
+if( ! $input->post('email')) {
+	$errors[] = "email required.";
+}
+if( ! $input->post('password')) {
+	$errors[] = "Password required.";
+}
+if(count($errors)){
+	return json_encode(array("success" => false, "errors" => $errors));	
+}
+
+$user_email = $sanitizer->email($input->post('email'));
+$user_data = array(
+	"email" => $user_email,
+	"password" => $input->post('password'),
+	"user" => $users->get('email=' . $this->sanitizer->selectorValue($user_email))
+);
+$fail_message = array("Login failed.");
+
+if( ! $user_data['user']->id) {
+	return json_encode(array("success" => false, "errors" => $fail_message));
+}
+
+$pcp = wire("modules")->get("ProcessContactPages");
+$username = $user_data['user']->name;
+$user = $pcp->login($username, $input->post('password'));
+
+if(is_string($user)){
+	// ProcessContactPages has returned an error string
+	return json_encode(array("success" => false, "errors" => array($user)));
+}
+if( ! is_object($user) || ! $user->id) {
+	return json_encode(array("success" => false, "errors" => $fail_message));
+}
+$cart = $this->modules->get("OrderCart");
+$num_cart_items = $cart->getNumCartItems($user->id);
+
+return json_encode(array("success" => true, "num_cart_items" => $num_cart_items, "message" => "Hi $username, welcome back."));
