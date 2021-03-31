2859d02f7fade5ddab335f293ed9cedcb53e595d
Paul Ashby <paul@primitive.co>
2021-03-31 18:03:30 +0100

    Render test forms from ProcessContactPages module

--- templates/home.php
+++ templates/home.php
@@ -1,7 +1,22 @@
 <?php namespace ProcessWire;
 
-// include("./includes/_main.php"); 
+$pcp = $modules->get("ProcessContactPages");
+			
+$pcp_forms = array(
+	array(
+		"title" => "Contact us",
+		"form" => "Contact Form",
+		"handler" => $urls->templates . "js/processcontactpages.js"
+	),
+	// array(
+	// 	"title" => "Register",
+	// 	"form" => "Registration Form",
+	// 	"handler" => $urls->templates . "js/processcontactpages.js" // including again as test
+	// )
+);
+$forms_markup = $pcp->renderForms($pcp_forms);
+
 echo "<main data-pw-id='main'>
-<h1>Home Page</h1>
+$forms_markup
 </main>";
 ?>
