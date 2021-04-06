e9776f5dda3099b84620cfb169febdb9f9d8aa6b
Paul Ashby <paul@primitive.co>
2021-04-06 18:47:31 +0100

    Set cancel label text to 'close'

--- templates/components/search.php
+++ templates/components/search.php
@@ -12,7 +12,7 @@ if($value["container"]){
 	$close = "</div>";
 }
 $submit_button = $files->render('components/buttons/formButton', ['button_class'=>'form__button form__button--submit', 'button_type'=>'submit', 'action'=>'search']);
-$cancel_button = $files->render('components/buttons/formButton', ['button_class'=>'form__button form__button--cancel', 'button_type'=>'button', 'action'=>'cancel']);
+$cancel_button = $files->render('components/buttons/formButton', ['button_class'=>'form__button form__button--cancel', 'button_type'=>'button', 'action'=>'cancel', 'label'=>'close']);
 
 	$out .= "<form class='form form--search' action='/' method='get'>
 		<div class='form__inputs'>
