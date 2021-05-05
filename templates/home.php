97d7e1927987fd2af9f62d510d7bd104aa0386ec
Paul Ashby <paul@primitive.co>
2021-05-05 09:20:30 +0100

    Remove contact form, replace with placeholder title

--- templates/home.php
+++ templates/home.php
@@ -1,22 +1,5 @@
 <?php namespace ProcessWire;
-
-$pcp = $modules->get("ProcessContactPages");
-			
-$pcp_forms = array(
-	array(
-		"title" => "Contact us",
-		"form" => "Contact Form",
-		"handler" => $urls->templates . "js/processcontactpages.js"
-	),
-	// array(
-	// 	"title" => "Register",
-	// 	"form" => "Registration Form",
-	// 	"handler" => $urls->templates . "js/processcontactpages.js" // including again as test
-	// )
-);
-$forms_markup = $pcp->renderForms($pcp_forms);
-
 echo "<main data-pw-id='main'>
-$forms_markup
+	<h1 class='page-title'>Welcome to paperbird</h1>
 </main>";
 ?>
