f98d8c5a4b2c09d582d4a7a5ae74a8c0b6feaa6f
Paul Ashby <paul@primitive.co>
2021-05-16 11:15:42 +0100

    Add h1 svg and random image from homepag image field

--- templates/home.php
+++ templates/home.php
@@ -1,5 +1,8 @@
 <?php namespace ProcessWire;
+
+$random_image_url = $page->image->getRandom()->url;
 echo "<main data-pw-id='main'>
-	<h1 class='page-title'>Welcome to paperbird</h1>
+	<h1 class='page-title page-title--home'>Welcome to paperbird</h1>
+	<img src='$random_image_url' class='home-image--mob'>
 </main>";
 ?>
