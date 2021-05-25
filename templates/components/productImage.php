d075b02ec7607f1c67dad388d04af62d2e94b733
Paul Ashby <paul@primitive.co>
2021-05-25 17:15:13 +0100

    Modify array and pass to LazyResponsiveImages module rather than creating a new array

--- templates/components/productImage.php
+++ templates/components/productImage.php
@@ -3,22 +3,20 @@
 if( ! isset($context)){ $context = "listing"; }
 
 // Listings images open the lightbox when clicked, cart images don't
-if( ! isset($data_attr_String)){ $data_attr_String = ""; }
+if( ! array_key_exists("product_data_attributes", $product_shot_options)) {
+	$product_shot_options["product_data_attributes"] = "";	
+}
+
+$product = $product_shot_options["product"];
 
 $product_shot = $product->product_shot->first();
 $dsc = $product_shot->description;
 $alt_str = $dsc ? $dsc : $product->title;
 
-$product_shot_options = [
-	"alt_str"=>$alt_str,
-	"class"=>$class,
-	"context"=>$context,
-	"field_name"=>$field_name,
-	"image"=>$product_shot,
-	"lazy_load"=>$lazy_load,
-	"product_data_attributes"=>$data_attr_String,
-	"sizes"=>$sizes,
-	"webp"=>true
-];
+$product_shot_options["alt_str"] = $alt_str;
+$product_shot_options["image"] = $product_shot;
+$product_shot_options["webp"] = true;
+
+$lazyImages = $product_shot_options["lazyImages"];
 
 return $lazyImages->renderImage($product_shot_options);
