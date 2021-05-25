57d6ca92e7f922f9b7901ea6419cad6c1c7e4bb8
Paul Ashby <paul@primitive.co>
2021-05-25 17:03:12 +0100

    Add hook to populate catagory_image_page field to allow images to be set for subcategory listings on main category pages

--- ready.php
+++ ready.php
@@ -1,9 +1,13 @@
 <?php
 
-// Populate artist->biography_card Page Reference field
 $wire->addHookAfter('InputfieldPage::getSelectablePages', function($event) {
 	if($event->object->hasField == 'biography_card') {
+		// Populate artist->biography_card Page Reference field
 		$selector = 'template=product, artist.title=' . $event->arguments('page')->title;
 		$event->return = $event->pages->find($selector);
-  }
+	} else if($event->object->hasField == 'category_image_page') {
+		// Populate category_sub->category_image_page Page Reference field
+		$selector = 'template=product, parent=' . $event->arguments('page');
+	    $event->return = $event->pages->find($selector);
+	  }
 });
