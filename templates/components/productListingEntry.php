093cb079449fc1d58f4c00a8c883e6662a0a7875
Paul Ashby <paul@primitive.co>
2021-05-25 17:16:48 +0100

    Modify array and pass to productImage rather than creating a new array

--- templates/components/productListingEntry.php
+++ templates/components/productListingEntry.php
@@ -1,29 +1,25 @@
 <?php namespace ProcessWire;
 
+//TODO: Do we actually need context?
 if( ! isset($context)){ $context = "listing";}
 
+$product = $product_shot_options["product"];
 $sku = $product->sku;
-$data_attr_String = "data-action='openLightbox' data-sku='$sku'";
+$pg_template = $page->template->name;
+$data_attr_String = $pg_template == "category-main" ? "" : "data-action='openLightbox' data-sku='$sku'";
 
 $entry_out = "<div class='products__product' $data_attr_String>";
 
-$product_shot_options = [
-	"class"=>$class,
-	"context"=>$context,
-	"data_attr_String"=>$data_attr_String,
-	"field_name"=>$field_name,
-	"product"=>$product,
-	"lazy_load"=>$lazy_load,
-	"lazyImages"=>$lazyImages,
-	"sizes"=>$sizes
-];
-$product_shot_markup = $this->files->render("components/productImage", $product_shot_options);
+$product_shot_options["product_data_attributes"] = $data_attr_String;
+$product_shot_markup = $this->files->render("components/productImage", Array("product_shot_options"=>$product_shot_options));
+
 $entry_out .= $product_shot_markup;
 
 $product_title_sku_options = [
 	"product"=>$product,
 	"data_attr_String"=>$data_attr_String
 ];
+
 $entry_out .= $this->files->render("components/productTitleSku", $product_title_sku_options);
 $entry_out .= "</div><!-- End Products__product -->";
 
