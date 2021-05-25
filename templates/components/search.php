a1c274d3da316a45317ed169b852d17c90de72c2
Paul Ashby <paul@primitive.co>
2021-05-25 17:19:26 +0100

    Load search results page instead of doing ajax call

--- templates/components/search.php
+++ templates/components/search.php
@@ -1,6 +1,7 @@
 <?php namespace ProcessWire;
 
-//TODO: Search results need to be scrollable with search form at top, not bottom as currently
+$urls = include $config->paths->templates . "utilities-urls.php";
+$search_url = $urls["search"];
 
 $out = "";
 $close = "";
@@ -14,7 +15,7 @@ if($value["container"]){
 $submit_button = $files->render('components/buttons/formButton', ['button_class'=>'form__button form__button--submit', 'button_type'=>'submit', 'action'=>'search']);
 $cancel_button = $files->render('components/buttons/formButton', ['button_class'=>'form__button form__button--cancel', 'button_type'=>'button', 'action'=>'cancel', 'label'=>'close']);
 
-	$out .= "<form class='form form--search' action='/' method='get'>
+	$out .= "<form class='form form--search' action='$search_url' method='get'>
 		<div class='form__inputs'>
 			<input class='form__input' type='text' name='q' id='search_query' autocomplete='off' placeholder='Search'/>
 			{$submit_button}
