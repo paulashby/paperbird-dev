d49ad3f10a21a1667e23908a9974d66ed80d5bf2
Paul Ashby <paul@primitive.co>
2021-05-28 15:52:14 +0100

    Remove extraneous page furniture

--- templates/error-page.php
+++ templates/error-page.php
@@ -1,16 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-		<title><?php echo $page->title; ?></title>
-		<link rel="stylesheet" type="text/css" href="<?php echo $config->urls->templates?>styles/main.css" />
-	</head>
-	<body>
-		<?php
-		$title = $page->title;
-		echo "<main data-pw-id='main'>
+<?php namespace ProcessWire;
+
+$title = $page->title;
+
+echo "<main data-pw-id='main'>
 			<h1 class='page-title'>$title</h1>
 		</main>";
-		?>
-	</body>
-</html>
