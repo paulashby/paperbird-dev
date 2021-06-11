3c8feeed4b6521d4c4c9af57f01e7bfec9c5deb5
Paul Ashby <paul@primitive.co>
2021-06-11 13:32:41 +0100

    Update maintenance message

--- templates/maintenance.php
+++ templates/maintenance.php
@@ -1,6 +1,6 @@
 <?php namespace ProcessWire;
 
 echo "<main data-pw-id='main'>
-	<h1 class='page-title'>Paper Bird is currently offline</h1>
-	<h2 class='status-message'>But check back soon to see our brand new site!</h2>
+	<h1 class='page-title'>Our brand new trade web site is on it&apos;s way</h1>
+	<h2 class='status-message'>Please check back soon! Meanwhile, you can get in touch via our <a href='https://www.etsy.com/uk/shop/PaperBirdGallery'>online etsy shop</a></h2>
 </main>";
