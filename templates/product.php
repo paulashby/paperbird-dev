8ff5863e8b5400981ecc363219d0a42a4b6abc36
Paul Ashby <paul@primitive.co>
2021-06-11 13:34:53 +0100

    Redirect direct access of product page to parent subcategory page

--- templates/product.php
+++ templates/product.php
@@ -1,15 +1,5 @@
 <?php namespace ProcessWire;
 
+$session->redirect($page->parent->url);
+
 $out = "<div>";
-
-$dsc = $page->product_shot->description;
-$alt_text = $dsc ? $dsc : $page->title;
-$sku = $page->sku;
-
-$out .= "<main data-pw-id='main'>
-<img src='" . $page->product_shot->first()->size(200,0)->url . "' alt='" . $alt_text . "'>
-	<p>$sku</p>
-	</div>
-	</main>";
-	
-echo $out;
