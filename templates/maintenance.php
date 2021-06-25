2006677038df363e9201f9332729ef923c6c201a
Paul Ashby <paul@primitive.co>
2021-06-25 10:37:29 +0100

    Add grouping divs for styling purposes

--- templates/maintenance.php
+++ templates/maintenance.php
@@ -1,6 +1,10 @@
 <?php namespace ProcessWire;
 
 echo "<main data-pw-id='main'>
-	<h1 class='page-title'>Our brand new trade web site is on it&apos;s way</h1>
-	<h2 class='status-message'>Please check back soon! Meanwhile, you can get in touch via our <a href='https://www.etsy.com/uk/shop/PaperBirdGallery'>online etsy shop</a></h2>
+<div class='maintenance-messages'>
+		<h1 class='page-title--home page-title--home-maintenance'>Our brand new trade web site is on its way</h1>
+		<div class=messages-secondary>
+			<h2 class='status-message'>Please check back soon!</h2><p class='maintenance-message'>Meanwhile, you can get in touch via our <a href='https://www.etsy.com/uk/shop/PaperBirdGallery'>etsy shop</a></p>
+		</div>
+	</div>
 </main>";
