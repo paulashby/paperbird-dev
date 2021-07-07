bbfe3afecfb5205917e4871547a7e0c094de5257
Paul Ashby <paul@primitive.co>
2021-07-07 13:21:06 +0100

    Manage inclusion of pages in admin tree according to user role

--- ready.php
+++ ready.php
@@ -11,3 +11,49 @@ $wire->addHookAfter('InputfieldPage::getSelectablePages', function($event) {
 	    $event->return = $event->pages->find($selector);
 	  }
 });
+
+// Restrict inclusion of admin page-list entries by role
+// guest doesn't have access to page-list, superuser gets everything
+$wire->addHookAfter('ProcessPageList::execute', function($event){
+
+	if($this->config->ajax){
+
+		if($this->user->hasRole("superuser")) return;
+
+		$role_pages = array(
+			"manager" => array(
+				"home",
+				"category-main",
+				"category-sub",
+				"product",
+				"generic-page",
+				"artists",
+				"artist",
+				"blog"
+			)
+		);
+		$showable = array();
+		$roles_array = $this->user->roles->implode(',', 'name');
+		$roles_array = explode(',', $roles_array);
+
+		foreach ($roles_array as $usr_role) {
+			if(array_key_exists($usr_role, $role_pages)) {
+				$showable = array_merge($showable, $role_pages[$usr_role]);
+			}
+		}
+		$event_data = json_decode($event->return, true);
+
+		foreach($event_data['children'] as $key => $child){
+
+		    $curr_entry = $this->pages->get($child['id']);
+		    $curr_template = $curr_entry->template; 
+
+		    if( ! in_array($curr_template, $showable)){
+		    	bd("unsetting " . $curr_template);
+		    	unset($event_data['children'][$key]);
+		    }
+		}
+		$event_data['children'] = array_values($event_data['children']);
+        $event->return = json_encode($event_data);
+    }
+});
