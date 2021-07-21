fba12d85e7b8155e362e68be9f2db669e571be75
Paul Ashby <paul@primitive.co>
2021-07-21 10:01:57 +0100

    Remove href from flip anchor. Set cursor to pointer for buttons

--- templates/components/buttons/flipButton.php
+++ templates/components/buttons/flipButton.php
@@ -2,4 +2,4 @@
 
 $action_attr = isset($action) ? $action : 'flip';
 
-return "<a href='#' class='{$button_class}' data-action='{$action_attr}' data-buttontype='{$button_type}'>{$button_text}</a>";
+return "<a class='{$button_class}' data-action='{$action_attr}' data-buttontype='{$button_type}'>{$button_text}</a>";
